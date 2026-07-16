794715361548518f68291fe1732948722931e9fd
somnath202 <somnathkhamkar3111@gmail.com>
2026-07-16 12:52:40 +0530

    Time: 0 ms (100%), Space: 12.4 MB (99.41%) - LeetHub

---
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root,int val = -1) {
+        if(root == NULL) return true;
+        if(val < 0) val = root->val ;
+        return root->val == val && isUnivalTree(root->left,val) && isUnivalTree(root->right,val);
+
+    }
+};
