398d5b0c4c0d8e28d5a8ca1a79c2ef25834477e9
Somnath Khamkar <somnathkhamkar3111@gmail.com>
2026-08-12 22:24:12 +0530

    Time: 41 ms (99.66%), Space: 149.3 MB (84.27%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -4,7 +4,7 @@ public:
         const int n=nums.size();
         int cnt=0;
         unordered_map<int, int> freq;//1 <= nums[i] <= 10^9
-        freq.reserve(n);
+        // freq.reserve(n);
         for (int l=0, r=0; r<n; r++){
             int x=nums[r];
             auto it=freq.find(x);
