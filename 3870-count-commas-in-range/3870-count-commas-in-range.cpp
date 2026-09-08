e4c4dd0271ac558839804ed4cfc854469a92ab92
Somnath Khamkar <somnathkhamkar3111@gmail.com>
2026-09-08 19:21:52 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        string s = to_string(n);
-        int size = s.size();
-        if(size < 4) return 0 ;
-        int ans = n -1000 +1 ;
-        return ans ;
-
+        return max(n-999,0);
     }
 };
