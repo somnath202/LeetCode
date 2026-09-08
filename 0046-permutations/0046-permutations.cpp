6d077d7416200cce9ee0eabcfd55992a1a1e5317
Somnath Khamkar <somnathkhamkar3111@gmail.com>
2026-09-09 00:29:49 +0530

    Time: 4 ms (9.82%), Space: 12.6 MB (6.6%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void help(vector<int>nums , int i , vector<int>&arr , vector<vector<int>>& ans , vector<int>used){
+        if(arr.size() == nums.size()){
+            ans.push_back(arr);
+            return ;
+        }
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(used[i]) continue ;
+            arr.push_back(nums[i]);
+            used[i] = 1 ;
+            help(nums,i , arr,ans,used);
+            arr.pop_back();
+            used[i] = 0 ;            
+        }
+
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>arr , used(nums.size(),0);
+        help(nums,0 , arr,ans,used);
+        return ans ;
+    }
+};
