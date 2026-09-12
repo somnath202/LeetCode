c6d41d295a1f0136cc05c148e76cbf85e53da1b0
Somnath Khamkar <somnathkhamkar3111@gmail.com>
2026-09-12 22:39:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (53.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n <= 1) return 1;
-
-        int prev2 = 1 ; 
-        int prev1 = 1 ;
-
-        for (int i = 2; i <= n; i++) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+    int fun(int n,vector<int>&dp){
+        if(n < 0) return  0;
+        if(n==0){
+            return 1;
         }
-
-        return prev1;
+        int left = 0, right = 0;
+        if(dp[n-1]!= -1) left = dp[n-1] ;
+        else {
+            left = fun(n-1,dp);
+            dp[n-1] = left ;
+        }
+        if( n-2 >= 0 && dp[n-2] != -1) right = dp[n-2] ;
+        else if(n-2 >= 0) {
+            right = fun(n-2,dp);
+            dp[n-2] = right ;
+        }
+        return left + right ;
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        for(auto it : dp) cout<<it<<" ";
+        return fun(n,dp);
+        
     }
 };
