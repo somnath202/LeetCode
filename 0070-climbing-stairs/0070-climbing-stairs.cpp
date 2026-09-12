05692650fa121e3cddf291d73044221eeeec3938
Somnath Khamkar <somnathkhamkar3111@gmail.com>
2026-09-12 22:50:54 +0530

    Time: 0 ms (100%), Space: 7.9 MB (74.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -12,9 +12,17 @@ public:
         return dp[n] = left + right ;
     }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        int ans = fun(n,dp);
-        for(auto it : dp) cout<<it<<" ";
-        return ans ;
+        // vector<int>dp(n+1,-1);
+        // int ans = fun(n,dp);
+        // for(auto it : dp) cout<<it<<" ";
+        // return ans ;
+        if(n == 1) return 1 ;
+        int a = 1 , b = 1 ;
+        for(int i = 2 ; i <= n ; i++){
+            int temp = b ;
+            b += a ;
+            a = temp ;
+        }
+        return b ;
     }
 };
