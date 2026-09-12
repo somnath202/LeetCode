c62424eb238e1880a9d62aece22ad6493d8ecc8b
Somnath Khamkar <somnathkhamkar3111@gmail.com>
2026-09-12 22:48:37 +0530

    Time: 3 ms (0.35%), Space: 8.7 MB (20.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
+    // Tabular form 
     int fun(int n,vector<int>&dp){
         if(n < 0) return  0;
         if(n==0){
             return 1;
         }
-        int left = 0, right = 0;
-        if(dp[n-1]!= -1) left = dp[n-1] ;
-        else {
-            left = fun(n-1,dp);
-            dp[n-1] = left ;
-        }
-        if( n-2 >= 0 && dp[n-2] != -1) right = dp[n-2] ;
-        else if(n-2 >= 0) {
-            right = fun(n-2,dp);
-            dp[n-2] = right ;
-        }
-        return left + right ;
+        if(dp[n] != -1) return dp[n] ;
+        int left = fun(n-1 , dp);
+        int right = fun(n-2,dp);
+        return dp[n] = left + right ;
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
+        int ans = fun(n,dp);
         for(auto it : dp) cout<<it<<" ";
-        return fun(n,dp);
-        
+        return ans ;
     }
 };
