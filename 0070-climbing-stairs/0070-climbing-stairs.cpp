cabc4b6751341b2f0fb4356ce10fd38dad5ea46f
Somnath Khamkar <somnathkhamkar3111@gmail.com>
2026-09-21 22:37:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -10,6 +10,14 @@ public:
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,0);
-        return  help(n,dp);
+        // return  help(n,dp);
+        if(n==1) return 1 ;
+        int x = 1 , y = 1 ;
+        for(int i = 2 ; i <=n ; i++){
+            int temp = x ;
+            x += y ;
+            y = temp ;
+        }
+        return x ;
     }
 };
